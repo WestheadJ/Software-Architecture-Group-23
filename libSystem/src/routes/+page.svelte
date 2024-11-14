b907c4797572aa3d30e63f0d44f7d1d3453acd8f
Reef-Jackson <reefworkmail@gmail.com>
2024-11-14 13:52:01 +0000

    Added a Navbar, Card, Hero Component. Cards are populatable thorugh props, and are rendered dynamically

--- libSystem/src/routes/+page.svelte
+++ libSystem/src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script>
   import Navbar from "$lib/components/Navbar.svelte";
   import Hero from "$lib/components/Hero.svelte";
+  import Card from "$lib/components/Card.svelte";
 </script>
 
 <Navbar />
 <Hero />
+<div class="w-full">
+  <h1 class="text-3xl font-semibold text-center mt-5">Books</h1>
+  <div class="w-full flex rounded">
+    <Card mediaTitle="War and Peace" mediaDesc="An example description" tags={['War', 'Non-fiction']}/>
+    <Card mediaTitle="War and Peace" mediaDesc="An example description" tags={['War', 'Non-fiction']}/>
+    <Card mediaTitle="War and Peace" mediaDesc="An example description" tags={['War', 'Non-fiction']}/>
+  </div>
+</div>
+
+
