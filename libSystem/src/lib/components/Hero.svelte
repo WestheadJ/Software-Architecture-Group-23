7772ee4eac18c8b4434c2229ebb1b9cb2cd59315
Reef-Jackson <reefworkmail@gmail.com>
2024-11-14 13:33:56 +0000

    Added Daisy UI and basic styling

---
+++ libSystem/src/lib/components/Hero.svelte
@@ -0,0 +1,11 @@
+<div class="hero bg-black min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">AML Library</h1>
+      <p class="py-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, suscipit. 
+      </p>
+      <button class="btn btn-primary">Seach for Content</button>
+    </div>
+  </div>
+</div>
---
+++ libSystem/src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Navbar from "$lib/components/Navbar.svelte";
+  import LoginForm from "$lib/components/LoginForm.svelte";
+</script>
+
+<Navbar />
+
+<!-- Remove Login Button on /Login -->
+<!-- Redirect to ./ upon successful login. -->
+
+<div class="w-screen h-screen flex">
+  <LoginForm />
+</div>
+
+
