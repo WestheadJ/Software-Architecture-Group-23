359dd947d92f6a9566e117b9f571cf4cdcbf970c
Reef-Jackson <reefworkmail@gmail.com>
2024-11-14 17:24:06 +0000

    Added basic login functionality

--- libSystem/src/stores/isAuthenticated.ts
+++
@@ -1,8 +0,0 @@
-import { writable, type Writable } from "svelte/store";
-import pb from '$lib/pocketbaseClient';
-
-export const isAuthenticated = writable(pb.authStore.isValid);
-
-pb.authStore.onChange(() => {
-    isAuthenticated.set(pb.authStore.isValid);
-});
