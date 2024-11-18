01df714ee79a54ef4d63390f301a5db91c14281b
Reef-Jackson <reefworkmail@gmail.com>
2024-11-18 10:58:41 +0000

    Moved functionality over to the Hooks.server.ts file, added basic registration functionality and stripped login, as it was reliant on external functionality inside /server which is now gone

--- libSystem/src/app.d.ts
+++ libSystem/src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { User } from "$lib/interfaces/User";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: PocketBase;
+			user: User;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
