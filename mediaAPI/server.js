f8fd0f203baf8e2e0bf7388b8ba485e77bd6fcf3
James Westhead <james_westhead@outlook.com>
2024-11-21 05:29:24 +0000

    Added media API creation

--- mediaAPI/server.js
+++ mediaAPI/server.js
@@ -2,24 +2,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { createClient } = require('@supabase/supabase-js');
 const dotenv = require('dotenv');
-dotenv.config();
+const nodeCache = require('node-cache')
+const cors = require('cors');
+const uuid = require('uuid');
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
 
 const supabase = createClient(process.env.URL, process.env.PUBLIC_KEY);
+
+const API_Keys_Cache = new nodeCache()
 
 app.get('/', async (req, res) => {
     res.send("hello world!");
 });
 
-app.get('/auth/token', async (req, res) => {
-    res.send("need an auth token?")
+app.post('/auth/token/get', async (req, res) => {
+    const email = req.body.email
+    const token = uuid.v4()
+    if (API_Keys_Cache.get(email) == undefined) {
+        API_Keys_Cache.set(email, token)
+
+        console.log("API key check", API_Keys_Cache.get(email))
+        res.send({ "status": "success", "token": token, "message": "successful token creation" })
+
+    }
+    else {
+        res.send({ "status": "error", "message": "Token already exists!" })
+    }
+
+
 })
+
+app.get('/auth/token/refresh', async (req, res) => { })
 
 app.get('/media/get/all', async (req, res) => {
     res.send("no items in the library")
