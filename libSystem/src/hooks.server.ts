d41feb57356bfb1d7d7aa64c3ef5c934df18703e
James Westhead <james_westhead@outlook.com>
2024-11-21 16:26:52 +0000

    FIX: connectivity issue to PB due to localhost issue

--- libSystem/src/hooks.server.ts
+++ libSystem/src/hooks.server.ts
@@ -2,7 +2,7 @@ import Pocketbase from 'pocketbase';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    event.locals.pb = new Pocketbase('http://localhost:8090');
+    event.locals.pb = new Pocketbase('http://127.0.0.1:8090');
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
     if (event.locals.pb.authStore.isValid) {
@@ -11,7 +11,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
     const response = await resolve(event);
 
-    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({secure: false }));
+    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }));
 
     return response;
 }
