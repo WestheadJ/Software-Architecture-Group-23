99dc819bed197998192e1529d56141ad54475eb1
James Westhead <james_westhead@outlook.com>
2024-12-07 03:56:17 +0000

    CSS changes to add a hover enlarge and highlight transition

--- libSystem/src/lib/components/Card.svelte
+++ libSystem/src/lib/components/Card.svelte
@@ -8,7 +8,9 @@
   export let imgUrl: string;
 </script>
 
-<div class="card bg-base-100 shadow-xl m-6 max-h-[40rem] flex flex-col">
+<div
+  class="card bg-base-100 shadow-xl m-6 max-h-[50rem] flex flex-col transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:shadow-blue-500/50"
+>
   <figure>
     <img class="w-full h-48 object-cover" src={imgUrl} alt={mediaTitle} />
   </figure>
