3f6a6c7d8ca60e47821b2e8abd617328f4dfe79c
James Westhead <james_westhead@outlook.com>
2024-12-13 22:30:41 +0000

    Added some arrows to show it's a collapsable

--- libSystem/src/lib/components/FilterCollapsable.svelte
+++ libSystem/src/lib/components/FilterCollapsable.svelte
@@ -6,11 +6,26 @@
 </script>
 
 <a
+    class="flex flex-row w-full justify-center text-left"
     onclick={() => {
         if (isFilterOpen) {
             isFilterOpen = false;
         } else isFilterOpen = true;
-    }}>{title}:</a
+    }}
+    >{title}
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5 transform transition-transform"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class:rotate-180={isFilterOpen}
+    >
+        <path
+            fill-rule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.193l3.71-3.963a.75.75 0 111.08 1.04l-4 4.25a.75.75 0 01-1.08 0l-4-4.25a.75.75 0 01.02-1.06z"
+            clip-rule="evenodd"
+        />
+    </svg></a
 >
 {#if isFilterOpen}
     <div class=" flex flex-col text-left p-3">
